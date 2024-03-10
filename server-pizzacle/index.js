9d79bcb10b3f78e3fa4af3fb5d97e1e68e1de8e9
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-03-10 09:37:20 +0100

    created route, controller and model for the user backend

---
+++ server-pizzacle/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require('cors')
+const userRouter = require("./routes/user.route")
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/', userRouter)
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on host ${PORT}`)
+})
---
+++ server-pizzacle/routes/user.route.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const {userRegister} = require("../controller/user.controller")
+
+router.post('/user/register', userRegister)
