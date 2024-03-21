27d3bbe79c6617de3aaf17a3dada9468e6a74f4a
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-03-21 21:58:34 +0100

    added cart icon and also help icons

--- interface-pizzacle/src/Components/Users/UserDashboard.jsx
+++ interface-pizzacle/src/Components/Users/UserDashboard.jsx
@@ -1,53 +1,60 @@
 import { Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const UserDashboard = () => {
-    const [tokenMatch, setTokenMatch] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const exploreRef = useRef(null);
+  const [tokenMatch, setTokenMatch] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [menu, setMenu] = useState([])
+  const exploreRef = useRef(null);
 
-  const TokenURL = 'http://localhost:3000/user/verifyToken';
-  // const ClassURL = 'http://localhost:3000/user/userDashboard';
-  const navigate = useNavigate()
+  const TokenURL = "http://localhost:3000/user/verifyToken";
+  const menuURL = 'http://localhost:3000/user/pizzaMenu';
+  const navigate = useNavigate();
   useEffect(() => {
     const checkToken = () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (!token) {
         setLoading(false);
-        navigate('/user/login');
+        navigate("/user/login");
       }
 
-      axios.post(TokenURL, { token })
-        .then(response => {
+      axios
+        .post(TokenURL, { token })
+        .then((response) => {
           if (token === response.data.token) {
             setLoading(false);
             setTokenMatch(true);
           } else {
             console.log("Token doesn't match");
-            navigate('/user/login');
+            navigate("/user/login");
             setLoading(true);
             setTokenMatch(false);
           }
         })
-        .catch(error => {
-          console.error('Error verifying token:', error);
+        .catch((error) => {
+          console.error("Error verifying token:", error);
           setLoading(false);
           setTokenMatch(false);
-          navigate('/user/login');
+          navigate("/user/login");
         });
     };
 
     const timeout = setTimeout(() => {
       if (loading) {
         setLoading(false);
-        navigate('/user/login');
+        navigate("/user/login");
       }
     }, 3000);
 
     checkToken();
+
+    axios.get(menuURL)
+    .then((response) => {
+      setMenu(response.data)
+      console.log(response.data.pizzaList[0]);
+    })
 
     return () => {
       clearTimeout(timeout);
@@ -55,54 +62,66 @@ const UserDashboard = () => {
   }, [navigate, loading]);
 
   const handleExploreClick = () => {
-    exploreRef.current.scrollIntoView({ behavior: 'smooth' });
+    exploreRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <>
-          <section className="bg-gray-200 h-full px-10 py-5">
-            <main className="bg-white p-20 pexels justify-center">
-              <h1 className="landingText text-white text-center mt-10 mb-10">
-                PIZZACLE
-              </h1>
-              <div className="space-x-10 flex justify-center">
-                <Button
-                  variant="contained"
-                  className="!bg-green-700 !p-3 w-60 text-white"
-                >
-                  Menu
-                </Button>
-                
-                <Button
-                  variant="contained"
-                  onClick={handleExploreClick}
-                  className="!bg-white  !p-3 w-60 !text-black"
-                >
-                  Explore
-                </Button>
-              </div>
-            </main>
-            <div ref={exploreRef}>
-            <main className="bg-white mt-10">
-              <div className="bg-green-900 w-full p-2 text-white text-center">
-                <p>You have a big appetite?</p>
-              </div>
-              <div className="h-screen "></div>
-            </main>
+      <section className="bg-gray-200 h-full px-10 py-5">
+        <main className="bg-white p-20 pexels justify-center">
+          <h1 className="landingText text-white text-center mt-10 mb-10">
+            PIZZACLE
+          </h1>
+          <div className="space-x-10 flex justify-center">
+            <Button
+              variant="contained"
+              className="!bg-green-700 !p-3 w-60 text-white"
+            >
+              Menu
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={handleExploreClick}
+              className="!bg-white  !p-3 w-60 !text-black"
+            >
+              Explore
+            </Button>
+          </div>
+        </main>
+        <div ref={exploreRef}>
+          <main className="bg-white mt-10">
+            <div className="bg-green-900 w-full p-2 text-white text-center">
+              <p>You have a big appetite?</p>
             </div>
+            <div className="h-screen ">
+             
+                  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                  <img
+                    class="w-full"
+                    src={menu.image_URL}
+                    alt="Sunset in the mountains"
+                  />
+                  
+                 
+                </div>
+              
+             
+            </div>
+          </main>
+        </div>
 
-            <main className="bg-white mt-10">
-              <div className="bg-green-900 w-full py-2 px-10 text-white flex justify-between">
-                <p>You want to step down your meal?</p>
+        <main className="bg-white mt-10">
+          <div className="bg-green-900 w-full py-2 px-10 text-white flex justify-between">
+            <p>You want to step down your meal?</p>
 
-                <Link to="/user/product/all">See more</Link>
-              </div>
-              <div className="h-screen "></div>
-            </main>
-          </section>
-      
+            <Link to="/user/product/all">See more</Link>
+          </div>
+          <div className="h-screen "></div>
+        </main>
+      </section>
     </>
-  )
+  );
 };
 
 export default UserDashboard;
--- interface-pizzacle/src/Components/Users/UserNavbar.jsx
+++ interface-pizzacle/src/Components/Users/UserNavbar.jsx
@@ -1,17 +1,14 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
+import { Link } from "react-router-dom";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const UserNavbar = () => {
-  const firstName = localStorage.getItem('firstName');
-  const lastName = localStorage.getItem('lastName');
-
-
+  const firstName = localStorage.getItem("firstName");
+  const lastName = localStorage.getItem("lastName");
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -25,20 +22,20 @@ const UserNavbar = () => {
   function stringToColor(string) {
     let hash = 0;
     let i;
-  
+
     /* eslint-disable no-bitwise */
     for (i = 0; i < string.length; i += 1) {
       hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
-  
-    let color = '#';
-  
+
+    let color = "#";
+
     for (i = 0; i < 3; i += 1) {
       const value = (hash >> (i * 8)) & 0xaf;
       color += `00${value.toString(16)}`.slice(-2);
     }
     /* eslint-enable no-bitwise */
-  
+
     return color;
   }
 
@@ -47,71 +44,71 @@ const UserNavbar = () => {
       sx: {
         bgcolor: stringToColor(name),
       },
-      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
     };
   }
-  
 
   return (
-   
     <>
-    <nav className="bg-green-800 lg:py-1 text-white text-center">
-      <small>We open at 9:00AM</small>
-    </nav>
-    <nav className="shadow-md lg:py-3 lg:px-10 flex justify-between items-center sticky top-0 bg-white z-10">
-      <Link to='/user/dashboard'>
-        <h1 className="logop">PIZZACLE</h1>
-      </Link>
-      <div className="flex items-center gap-2">
-        <div className="border-2 border-gray-200 rounded-md px-5">
-          <input type="text" placeholder="Search for pizza, side dish and drinks" className="outline-none border-none p-2"/>
+      <nav className="bg-green-800 lg:py-1 text-white text-center">
+        <small>We open at 9:00AM</small>
+      </nav>
+      <nav className="shadow-md lg:py-3 lg:px-10 flex justify-between items-center sticky top-0 bg-white z-10">
+        <Link to="/user/dashboard">
+          <h1 className="logop">PIZZACLE</h1>
+        </Link>
+        <div className="flex items-center gap-2">
+          <div className="border-2 border-gray-200 rounded-md px-5">
+            <input
+              type="text"
+              placeholder="Search for pizza, side dish and drinks"
+              className="outline-none border-none p-2"
+            />
+          </div>
+
+          <span className="bg-green-700 w-full p-2 rounded-md text-white text-center shadow-md me-10">
+            <button>Search</button>
+          </span>
+
+          <div className="space-x-10 flex items-center">
+            <Link to="/user/cart" className="flex items-center gap-2">
+              <span class="material-symbols-outlined">shopping_cart</span>
+              <span>Cart</span>
+            </Link>
+            <Link to="/user/cart" className="flex items-center gap-2">
+              <span class="material-symbols-outlined">help</span>
+              <span>Help</span>
+            </Link>
+
+            <div
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <Stack direction="row" spacing={2}>
+                <Avatar {...stringAvatar(`${firstName} ${lastName}`)} />
+              </Stack>
+            </div>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Orders</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
         </div>
-        
-      <span className="bg-green-700 w-full p-2 rounded-md text-white text-center shadow-md me-10">
-          <button >Search</button>
-      </span>
-
-      <div className="space-x-10 flex items-center">
-      <Link to='/user/cart'>
-         <p>Cart</p>
-        </Link>
-        <Link to='/user/cart'>
-         <p>Help</p>
-        </Link>
-
-
-        <div
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        <Stack direction="row" spacing={2}>
-      <Avatar {...stringAvatar(`${firstName} ${lastName}`)} />
-    </Stack>
-      </div>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>Orders</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-
-        
-      </div>
-      </div>
-    </nav>
+      </nav>
     </>
-  
-  )
-}
+  );
+};
 
-export default UserNavbar
+export default UserNavbar;
