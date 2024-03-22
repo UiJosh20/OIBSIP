038d710973b48542977787ec5a34d6d1075136bf
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-03-22 18:50:47 +0100

    working on adding to cart page

--- interface-pizzacle/src/Components/Users/ProductDescription.jsx
+++ interface-pizzacle/src/Components/Users/ProductDescription.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
@@ -6,6 +7,7 @@ const ProductDescription = () => {
   const { id } = useParams()
   const menuURL = 'http://localhost:3000/user/pizzaMenu';
     const [description, setPizzaDescription] = useState([])
+    const [quantity, setQuantity] = useState(1); 
 
     useEffect(() => {
         axios.get(menuURL)
@@ -17,21 +19,47 @@ const ProductDescription = () => {
           console.error("Error fetching pizza:", error);
         });
     }, [id]);
+
+    const incrementQuantity = () => {
+      setQuantity(quantity + 1);
+    };
+  
+    const decrementQuantity = () => {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+      }
+    };
+
+    const addToCart = () => {
+      alert("added to cart")
+    };
+
+
   return (
     <>
-    <section className="bg-gray-200 h-full px-5 py-5">
+    <section className="bg-gray-200 h-full px-5 py-5 flex gap-5">
         <main className="bg-white lg:flex lg:items-center">
           <div>
             <img src={description.image_URL} alt="" />
           </div>
           <div>
             <h1 className="text-3xl font-bold mb-3">{description.name}</h1>
-            <p className="text-gray-500 mb-3">Price : ${description.price}</p>
-            <p className="text-gray-500 mb-3">Description : {description.description}</p>
-            
+            <p className="text-gray-500 mb-3">Price: ${description.price}</p>
+            <p className="text-gray-500 mb-3">Description: {description.description}</p>
+            <div className="flex items-center">
+              <button className='bg-gray-500 text-white px-3 py-1 rounded-l-md me-4' onClick={decrementQuantity}>-</button>
+              <span>{quantity}</span>
+              <button onClick={incrementQuantity} className='bg-black text-white px-3 py-1 rounded-r-md ms-4'>+</button>
+            </div>
+            <div className='pe-10'>
+              <Button onClick={addToCart} className='bg-green-700 text-white p-4 rounded-md mt-4 shadow-md w-full'>Add to Cart</Button>
+            </div>
           </div>
         </main>
-    </section>
+        <main className='bg-white px-5 py-5 w-96'>
+
+        </main>
+      </section>
     </>
   )
 }
