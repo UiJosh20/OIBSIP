42c9e5814653bda77258293fd78a7b0fe6a0c3ed
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-03-22 16:00:16 +0100

    fixed the product on the user interface

--- interface-pizzacle/src/Components/Layout/Description.jsx
+++ interface-pizzacle/src/Components/Layout/Description.jsx
@@ -20,15 +20,15 @@ const Description = () => {
   return (
     <>
     <section className="bg-gray-200 h-full px-5 py-5">
-        <main className="bg-white">
+        <main className="bg-white lg:flex lg:items-center">
           <div>
             <img src={description.image_URL} alt="" />
           </div>
           <div>
             <h1 className="text-3xl font-bold mb-3">{description.name}</h1>
-            <p className="text-gray-500 mb-3">{description.price}</p>
-            <p className="text-gray-500 mb-3">{description.description}</p>
-            <Link to="/menu" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"></Link>
+            <p className="text-gray-500 mb-3">Price : {description.price}</p>
+            <p className="text-gray-500 mb-3">Description : {description.description}</p>
+            
           </div>
         </main>
     </section>
