9a13089f5306adc2e65324f1effcc80e03dbb81e
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-03-27 18:31:30 +0100

    home screen responsiveness

--- interface-pizzacle/src/Components/Layout/Home.jsx
+++ interface-pizzacle/src/Components/Layout/Home.jsx
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <>
       <section className="bg-black h-screen text-white flex  !overflow-y-hidden bgImg">
-        <video src="https://assets.mixkit.co/videos/preview/mixkit-cooking-pizza-in-a-traditional-oven-32415-large.mp4" loop autoPlay className="w-full absolute lg:block hidden"></video>
-        <div className="motto lg:mt-32 z-10 mt-5">
+        {/* <video src="https://assets.mixkit.co/videos/preview/mixkit-cooking-pizza-in-a-traditional-oven-32415-large.mp4" loop autoPlay className="w-full absolute lg:block hidden"></video> */}
+        <div className="motto lg:mt-20 z-10 mt-5 lg:text-left text-center lg:px-20 p-3">
           <h1>Pizza at it's overall pinnacle</h1>
           <p>We deliver anywhere, with an all-round speed...</p>
 
-          <Link to='/user/dashboard' className="button">
-          <button  className="bg-green-800 p-3 rounded-md mt-3 w-80">Order now</button>
+          <Link to='/user/login' className="button">
+          <button  className="bg-green-800 p-3 rounded-md lg:mt-3  mt-20 w-80">Order now</button>
           </Link>
         </div>
 
--- interface-pizzacle/src/Components/Layout/Navbar.jsx
+++ interface-pizzacle/src/Components/Layout/Navbar.jsx
@@ -1,12 +1,11 @@
-
-import * as React from 'react';
+import * as React from "react";
 import { useEffect, useState } from "react";
-import { Link} from "react-router-dom"
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import { ListItem, ListItemButton, ListItemIcon } from '@mui/material';
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import { ListItem, ListItemButton, ListItemIcon } from "@mui/material";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
@@ -16,9 +15,14 @@ const Navbar = () => {
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)} className=" !w-48">
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      className="!w-48 "
+    >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon></ListItemIcon>
@@ -31,30 +35,48 @@ const Navbar = () => {
 
   return (
     <>
+      <nav className="bg-green-800 lg:py-1 text-white text-center ">
+        <small>We open at 9:00AM</small>
+      </nav>
+      <nav className="shadow-md lg:py-3 lg:px-10 flex justify-between items-center sticky top-0 bg-black text-white z-50 p-3">
+        <Link to="/home">
+          <h1 className="logop">PIZZACLE</h1>
+        </Link>
 
+        <div className="lg:hidden block">
+          <span
+            class="material-symbols-outlined cursor-pointer"
+            onClick={toggleDrawer(true)}
+          >
+            menu
+          </span>
 
-<nav className="bg-green-800 lg:py-1 text-white text-center ">
-      <small>We open at 9:00AM</small>
-    </nav>
-    <nav className="shadow-md lg:py-3 lg:px-10 flex justify-between items-center sticky top-0 bg-black text-white z-50 p-3">
-      <Link to="/home">
-        <h1 className="logop">PIZZACLE</h1>
-      </Link>
-      
-
-      <Button onClick={toggleDrawer(true)} className='lg:hidden block text-white'>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)} className='lg:hidden block'>
-        {DrawerList}
-      </Drawer>
-      <div className="space-x-5 lg:block hidden">
-      <Link to='/blog' className="homelink">Blog</Link>
-          <Link to='/product' className="homelink">Our product</Link>
-          <Link to='/user/register' className="homelink">Become a customer</Link>
-          <Link to='/user/login' className="btn bg-green-700 p-2 w-20 rounded-md text-center">Login</Link>
-      </div>
-
-     
-    </nav>
+          <Drawer
+            open={open}
+            onClose={toggleDrawer(false)}
+            className="lg:hidden bloc"
+          >
+            {DrawerList}
+          </Drawer>
+        </div>
+        <div className="space-x-5 lg:block hidden">
+          <Link to="/blog" className="homelink">
+            Blog
+          </Link>
+          <Link to="/product" className="homelink">
+            Our product
+          </Link>
+          <Link to="/user/register" className="homelink">
+            Become a customer
+          </Link>
+          <Link
+            to="/user/login"
+            className="btn bg-green-700 p-2 w-20 rounded-md text-center"
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
       {/* <nav className=" flex lg:px-10 lg:py-4 items-center justify-between border-t-2 border-green-700 bg-black text-white fixed w-full z-50">
         <Link to="/">
           <h1 className="logop">PIZZACLE</h1>
@@ -67,9 +89,8 @@ const Navbar = () => {
         </ul>
 
       </nav> */}
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
-
+export default Navbar;
